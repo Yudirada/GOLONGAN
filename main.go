db3368abd7e6704fabf6165828a12575d55cce19
Adri Yudianto <adriyudianto@gmail.com>
2017-05-17 14:22:56 +0700

    move all to glib

--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/yudirada/golongan-lib/p"
+	glib "github.com/yudirada/golongan-lib"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	r := gin.New()
-	r.Use(gin.Logger())
-	//router.LoadHTMLGlob("templates/*.tmpl.html")
-	//router.Static("/static", "static")
-
-	//router.GET("/", func(c *gin.Context) {
-	//	c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	//})
-
-	p.RenderIndex(r)
-
-	r.Run(":" + port)
+	glib.run()
 }
