aa09815f144b6b57deb55c23adeb1d1de8b8b2e3
Adri Yudianto <adriyudianto@gmail.com>
2017-05-17 10:35:03 +0700

    edit main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -17,11 +16,14 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	//router.Static("/static", "static")
 
+	//router.GET("/", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	//})
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.Writer.Write([]byte("TEST"))
 	})
 
 	router.Run(":" + port)
