c894a22dfa9c3dd8ecbfabbfbac181a1a73adb63
Adri Yudianto <adriyudianto@gmail.com>
2017-05-17 12:24:20 +0700

    change vendor.json

---
+++ vendor/github.com/yudirada/golongan/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yudirada/golongan/p"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	r := gin.New()
+	r.Use(gin.Logger())
+	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	//router.Static("/static", "static")
+
+	//router.GET("/", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	//})
+
+	p.GenPageIndex(r)
+
+	r.Run(":" + port)
+}
