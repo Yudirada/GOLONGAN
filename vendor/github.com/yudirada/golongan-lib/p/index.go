734c9017c3f4e65d0489bc881c71ad7079b0a9da
Adri Yudianto <adriyudianto@gmail.com>
2017-05-22 10:03:55 +0700

    add gtm page

--- vendor/github.com/yudirada/golongan-lib/p/index.go
+++ vendor/github.com/yudirada/golongan-lib/p/index.go
@@ -9,11 +9,19 @@ import (
 // RenderIndex contains router for index
 func RenderIndex(r *gin.Engine) {
 	r.GET("/", pageIndex)
+	r.GET("/GTM", pageGTM)
 }
 
 // PageIndex render Index page
 func pageIndex(c *gin.Context) {
 	//c.Writer.Write([]byte("TEST3"))
 	c.HTML(http.StatusOK, "d_login.html", nil)
+
+}
+
+// PageGTM render Google Tag Manager test
+func pageGTM(c *gin.Context) {
+	//c.Writer.Write([]byte("TEST3"))
+	c.HTML(http.StatusOK, "t_gtm.html", nil)
 
 }
